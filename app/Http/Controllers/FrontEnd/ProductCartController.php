3d451109925368852804aa4a7cedf07e9a2abcb4
Akash0000439 <akashlog402@gmail.com>
2024-02-04 14:29:14 +0600

    without login order

--- app/Http/Controllers/FrontEnd/ProductCartController.php
+++ app/Http/Controllers/FrontEnd/ProductCartController.php
@@ -16,11 +16,11 @@ class ProductCartController extends Controller
 {
     public function addToCart()
     {
-        if (!Auth::check()) {
-            Alert::error('Please Login First');
-            return redirect()->route('customer_login');
-        }
-        $user_id = auth()->user()->id;
+        // if (!Auth::check()) {
+        //     Alert::error('Please Login First');
+        //     return redirect()->route('customer_login');
+        // }
+        $user_id = auth()->user()->id ?? null;
         $carts = ProductCart::where('user_id', $user_id)->with('product')->get();
         $totalprice = ProductCart::where('user_id', Auth::id())
             ->get()
@@ -32,10 +32,12 @@ class ProductCartController extends Controller
 
     public function productAddCart(Request $request)
     {
+
+        $sessionId = session()->getId();
         $data = $request->all();
-        $user = Auth::user();
+        $user = Auth::user() ?? null;
         $product = Product::findOrFail(intval($data['productid']));
-        $existingCartItem = ProductCart::where('user_id', $user->id)
+        $existingCartItem = ProductCart::where('session_id', $sessionId)
             ->where('product_id', $product->id)
             ->first();
         if ($existingCartItem) {
@@ -43,7 +45,8 @@ class ProductCartController extends Controller
             $existingCartItem->save();
         } else {
             $cart = new ProductCart();
-            $cart->user_id = $user->id;
+            $cart->user_id = $user->id ?? null;
+            $cart->session_id = $sessionId;
             $cart->product_id = $product->id;
             $cart->quantity = 1;
             if ($product->discount_amount != null) {
@@ -54,9 +57,9 @@ class ProductCartController extends Controller
             $cart->order_type = 'REG';
             $cart->save();
         }
-        $product_count = ProductCart::where('user_id', $user->id)->count();
+        $product_count = ProductCart::where('session_id',$sessionId)->orWhere('user_id', $user->id ?? null)->count();
 
-        return response()->json(['message' => 'working', 'product_count' => $product_count]);
+        return response()->json(['message' => 'working','product_count'=> $product_count]);
     }
 
     public function productDeleteCart($id)
@@ -67,14 +70,15 @@ class ProductCartController extends Controller
     }
     public function productIncrement(Request $request)
     {
+        $sessionId = session()->getId();
         $data = $request->all();
-        $existingCartItem = ProductCart::where('user_id', Auth::id())->where('id', $data['productid'])->first();
+        $existingCartItem = ProductCart::where('session_id', $sessionId )->where('id', $data['productid'])->first();
         $unitPrice = $existingCartItem->unit_price;
         if ($existingCartItem) {
             if ('increment' == $data['type']) {
                 $existingCartItem->quantity += 1;
                 $existingCartItem->save();
-                $totalprice = ProductCart::where('user_id', Auth::id())
+                $totalprice = ProductCart::where('session_id', $sessionId )
                     ->get()
                     ->sum(function ($item) {
                         return $item->unit_price * $item->quantity;
@@ -85,14 +89,14 @@ class ProductCartController extends Controller
                 }
                 $existingCartItem->quantity -= 1;
                 $existingCartItem->save();
-                $totalprice = ProductCart::where('user_id', Auth::id())
+                $totalprice = ProductCart::where('session_id', $sessionId )
                     ->get()
                     ->sum(function ($item) {
                         return $item->unit_price * $item->quantity;
                     });
             }
-            $user_id = Auth::user()->id;
-            $totaldiscount = ProductCart::where('user_id', $user_id)
+            // $user_id = Auth::user()->id ?? null;
+            $totaldiscount = ProductCart::where('session_id', $sessionId )
             ->with('product')
             ->get()
             ->sum(function ($item) {

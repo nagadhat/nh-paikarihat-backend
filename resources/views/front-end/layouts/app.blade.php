4915109c1c1b834fc980383dbe648fecb8713c4c
Akash0000439 <akashlog402@gmail.com>
2024-02-20 11:10:31 +0600

    upload btn css

--- resources/views/front-end/layouts/app.blade.php
+++ resources/views/front-end/layouts/app.blade.php
@@ -126,7 +126,8 @@
      <link rel="stylesheet" href="{{ asset('front-end/assets/css/toastr.min.css') }}">
 
      <link rel="stylesheet" type="text/css" href="{{ asset('front-end/assets/css/redesign-new.css') }}" />
-     <link rel="stylesheet" type="text/css" href="{{ asset('front-end/assets/css/product-button.css') }}" />
+     {{-- <link rel="stylesheet" type="text/css" href="{{ asset('front-end/assets/css/product-button.css') }}" /> --}}
+     <link rel="stylesheet" type="text/css" href="{{ asset('front-end/assets/css/product-btn.css') }}" />
      <link rel="stylesheet" type="text/css" href="{{ asset('front-end/assets/css/login_page.css') }}" />
      <link rel="stylesheet" type="text/css" href="{{ asset('front-end/assets/css/customer-dashboard.css') }}" />
      <link rel="stylesheet" type="text/css" href="{{ asset('front-end/assets/css/customer-left-sidebar.css') }}" />

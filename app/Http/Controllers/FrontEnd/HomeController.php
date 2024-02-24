7cfa1bb28bc8bcd2434353a7c1e0b68f67d7db7d
Akash0000439 <akashlog402@gmail.com>
2024-02-24 12:28:52 +0600

    add search by product with word

--- app/Http/Controllers/FrontEnd/HomeController.php
+++ app/Http/Controllers/FrontEnd/HomeController.php
@@ -12,36 +12,29 @@ class HomeController extends Controller
     {
         $products = Product::latest()->limit(20)->get();
         // dd($products);
-        return view('front-end.home.home-page',compact('products'));
+        return view('front-end.home.home-page', compact('products'));
     }
 
     public function searchProduct(Request $request)
     {
-        $product_search = $request->input('productsearch');
-    
-        // Check if the search query starts with a letter or number
+        $product_search = strtolower($request->input('productsearch'));
         $firstChar = substr($product_search, 0, 1);
         if (ctype_alpha($firstChar)) {
-            // Search by title if the first character is a letter
-            $products = Product::where('title', 'LIKE', "$product_search%")
+            $products = Product::whereRaw('LOWER(title) LIKE ?', ["%$product_search%"])
                 ->latest()
                 ->get();
         } elseif (is_numeric($firstChar)) {
-            // Search by SKU if the first character is a number
             $products = Product::where('sku', 'LIKE', "$product_search%")
                 ->latest()
                 ->get();
         } else {
-            // Default search by both title and SKU
-            $products = Product::where(function($query) use ($product_search) {
-                    $query->where('title', 'LIKE', "%$product_search%")
+            $products = Product::where(function ($query) use ($product_search) {
+                $query->whereRaw('LOWER(title) LIKE ?', ["%$product_search%"])
                     ->orWhere('sku', 'LIKE', "%$product_search%");
-                })
-                ->latest() 
+            })
+                ->latest()
                 ->get();
         }
         return view('front-end.home.home-page', compact('products'));
     }
-    
-    
 }
